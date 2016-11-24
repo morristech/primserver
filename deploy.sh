974b3725771c643950421973fabb48a8b8a79d1e
Subin Sebastian <subin@xtel.in>
2016-11-25 00:15:40 +0530

    Work in progress : do not deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+echo "Deploying to prim server"
+git pull
+go get -u github.com/fogleman/primitive
+
+# kill node
+# start node index.js
--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
+var bodyParser  = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
+
+app.post('/queue/add', function(req, res) {
+    var fileKey = req.body.filekey;
+    var etag = req.body.etag;
+console.log(fileKey);
+    res.json({message: fileKey});
+});
 
 app.get('/', function (req, res) {
-  res.json({message : 'PRIMITIVE API v1.0'});
+  res.json({message : 'Primtive Internal API v1.0', value: 'You should not be seeing this'});
 })
 
 app.listen(55000, function () {
-  console.log('Running on 55000');
+  console.log('Listening on 55000');
 });
