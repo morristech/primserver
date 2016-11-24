d67a5ae164176899ea62272e955af54b2a5a24ae
Subin Sebastian <subin@xtel.in>
2016-11-24 21:24:10 +0530

    Bug fix

--- server/index.js
+++ server/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.json({message : 'PRIMITIVE API v1.0'})'
+  res.json({message : 'PRIMITIVE API v1.0'});
 })
 
 app.listen(55000, function () {
-  console.log('Running on 55000')
+  console.log('Running on 55000');
 });
