819dd79dbf1d52cc8ffe708cb7a8cf0a437333b1
Subin Sebastian <subin@xtel.in>
2016-11-25 11:43:40 +0530

    Added rm files

---
+++ primitive/app/src/main/java/com/napster/primitive/services/MessagingService.java
@@ -0,0 +1,11 @@
+package com.napster.primitive.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by napster on 25/11/16.
+ */
+
+public class MessagingService extends FirebaseMessagingService {
+
+}
