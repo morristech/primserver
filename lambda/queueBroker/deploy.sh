54623fbbd38780f0002047295246888dd43b9d0d
Subin Sebastian <subin@xtel.in>
2016-12-20 14:04:21 +0530

    Zip command made silent by directing output to /dev/null

--- lambda/queueBroker/deploy.sh
+++ lambda/queueBroker/deploy.sh
@@ -1,3 +1,3 @@
-zip -r out.zip -xi index.js node_modules/
+zip -r out.zip -xi index.js node_modules/ > /dev/null
 aws lambda update-function-code --function-name queueBroker --zip-file fileb://out.zip
 rm out.zip
