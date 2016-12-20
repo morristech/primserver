07e8b4d3cb5663eaeffd0d8f0beec9b7e15d6c59
Subin Sebastian <subin@xtel.in>
2016-12-20 14:12:19 +0530

    Cleanup

--- lambda/queueBroker/deploy.sh
+++ lambda/queueBroker/deploy.sh
@@ -1,3 +1,6 @@
+echo "Compressing Files"
 zip -r out.zip -xi index.js node_modules/ > /dev/null
+echo "Uploading to lambda"
 aws lambda update-function-code --function-name queueBroker --zip-file fileb://out.zip
+echo "Cleaning up"
 rm out.zip
